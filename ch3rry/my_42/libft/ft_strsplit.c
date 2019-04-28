8d7c3b5383706bedd4dfe4600c0812b073add1f6
Sergey Kharlamov <ch3rryhq@Sergeys-MacBook-Air.local>
2019-04-29 01:49:49 +0300

    ft_strsplit WIP

---
+++ ch3rry/my_42/libft/ft_strsplit.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/28 14:37:51 by caellis           #+#    #+#             */
+/*   Updated: 2019/04/29 01:48:13 by caellis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_count_words(const char *s, char c)
+{
+	int		w_c;
+	int		cue;
+	
+	cue = 0;
+	w_c = 0;
+	while (*s)
+	{
+		if (ft_memcmp(s, &c, 1))
+		{
+			if (cue == c)
+				w_c++;
+		}
+		cue = *s++;
+	}
+	return (w_c);
+}
+
+char	**ft_strsplit(char const *s, char c)
+{
+	char	**split;
+	int		cue;
+	int		w_c;
+	int		w_l;
+
+	split = NULL;
+	cue = 0;
+	w_c = 0;
+	if (s && c)
+	{
+		if ((split = (char **)malloc(sizeof(s) * (ft_count_words(s, c) + 1))))
+		{
+			while (*s)
+			{
+				w_l = 0;
+				while (ft_memcmp(s, &c, 1))
+				{
+					if (cue == *s++)
+						w_c++;
+					w_l++;
+				}
+				if (w_l && w_c)
+				{
+					split[w_c - 1] = ft_strncpy(ft_strnew(w_l), (s - w_l), w_l);
+					w_l = 0;				
+				}
+				cue = *s++;
+			}
+		}
+	}
+	return (split);
+}
