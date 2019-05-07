1bbbc8454995c1d7986e1c107d8f41f6e4908ed6
Caryl Ellis <caellis@mi-t6.21-school.ru>
2019-05-07 17:59:32 +0300

    gnl started

--- ch3rry/my_42/get_next_line/get_next_line.c
+++ ch3rry/my_42/get_next_line/get_next_line.c
@@ -3,38 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: caellis <caellis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 21:45:25 by caellis           #+#    #+#             */
-/*   Updated: 2019/05/06 11:44:21 by caellis          ###   ########.fr       */
+/*   Updated: 2019/05/07 17:59:03 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "../libft/libft.h"
+#include <stdio.h>	//DELETE_ME
 
-static void	*read_line(t_list	**buff, char **line)
+static int	read_line(t_list	**buff, char **line, int mode)
 {
-	
+	return (NULL);	
 }
 
-int			 get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	static t_list	*buff;
+	t_list			*selecta;
+	int				mode;
 
-	if (buff)
-	buff = ft_lstnew(NULL, 0);
-		printf("yes there is no *buff on the 1st launch");
+	if (fd > -1 && line && *line)
+		return (-1);
+	if (!(buff = (t_list*)malloc(sizeof(t_list)))
+			return (-1);
+	}
+	if (buff->content_size == (size_t)fd)
+		return (read_line(&buff, line, (mode=READF_L)));
+	while ((buff = buff->next))
+	{
+		if (buff->content_size == (size_t)fd)
+			return(read_line(&selecta, line, (mode=READF_L)));
+	}
+	ft_lstadd(&buff, ft_lstnew(&buff, fd));
+	return(read_line(&buff, line, (mode=READF_I));
+
 	return (1);
 }
 
-int			main()
+int			main(void)
 {
 	char	line[1000];
 	int		fd;
 
 	fd = open("get_next_line.h", O_RDONLY);
-	get_next_line(fd, &line);
+	get_next_line(fd, (char**)&line);
 	printf("%s\n", line);
 	return (0);
 }
--- ch3rry/my_42/get_next_line/get_next_line.h
+++ ch3rry/my_42/get_next_line/get_next_line.h
@@ -3,26 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: caellis <caellis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 21:45:29 by caellis           #+#    #+#             */
-/*   Updated: 2019/05/05 22:43:58 by caellis          ###   ########.fr       */
+/*   Updated: 2019/05/07 17:37:31 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdlib.h>
+#ifndef GNL_H
+# define GNL_H
+
+#ifndef LIBFT_H
+#include "../libft/libft.h"
+#endif
+
 # include <fcntl.h>
 # include <sys/types.h>
 # include <sys/uio.h>
-# include <unistd.h> 
 
 # define BUFF_SIZE 65536
 
+enum e_modes
+{
+	READF_L,
+	READF_I
+};
+
 /*
-typedef struct		s_list
+typedef struct		s_file
 {
 	void			*content;
 	size_t			content_size;
 	struct s_list	*next;
 }					t_list;
 */
+
+#endif
