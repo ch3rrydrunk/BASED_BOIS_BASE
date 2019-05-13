7f939ecf4c59e89cf5e3f924ff37361209b34129
Sergey Kharlamov <ch3rryhq@Sergeys-MacBook-Air.local>
2019-05-13 18:37:55 +0300

    this is so latenight omg

--- ch3rry/my_42/get_next_line/get_next_line.c
+++ ch3rry/my_42/get_next_line/get_next_line.c
@@ -6,22 +6,22 @@
 /*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 21:45:25 by caellis           #+#    #+#             */
-/*   Updated: 2019/05/13 14:18:42 by caellis          ###   ########.fr       */
+/*   Updated: 2019/05/13 15:51:45 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>	//DELETE_ME
 
-static int	read_line(t_list	**buff, char **line)
+static int	read_line(t_list	**files, char **line)
 {
 	char	*temp[BUFF_SIZE];
 	long	rb;
 
-	// проверить пустой ли buff->content
+	// проверить пустой ли files->content
 		// если пустой (указывает на Нуль или начинается с \0): выделить память (BUFF_SIZE)
-		// (ecли выделена память) считываем из входного_потока в buff->content
-		// ПОСЛЕ считывания (целиком) копируем из buff->content в line пока не встретим \n или \0
+		// (ecли выделена память) считываем из входного_потока в files->content
+		// ПОСЛЕ считывания (целиком) копируем из files->content в line пока не встретим \n или \0
 			// ПОСЛЕ считывания кидаем 
 	// 
 	return (0);	
@@ -29,48 +29,36 @@ static int	read_line(t_list	**buff, char **line)
 
 int			get_next_line(const int fd, char **line)
 {
-	static t_list	*buff;
-	int				rb;
-	char			buf[BUFF_SIZE + 1];
-	char			*cue;
+	static t_list	*files;
+	char			buff[BUFF_SIZE + 1]
 
-	// Error-check + List creation
 	if (!(fd > -1 && line && BUFF_SIZE > 0))
 		return (-1);
-	if (!(buff))
+	if (!(files))
 	{
-		if (!(buff = ft_lstnew(NULL, 0)))
+		if (!(files = ft_lstnew(ft_strnew(BUFF_SIZE), (size_t)fd)))
 			return (-1);
-		buff->content_size = (size_t)fd;
 	}
-
-	// find right node
-	if (buff->content_size == (size_t)fd) 
-		{
-			// read to current node
-			return (read_line(&buff, line));
-		}
-	while ((buff = buff->next))
+	if (files->content_size == (size_t)fd) 
+			return (read_line(&files, line));
+	while ((files = files->next))
 	{
-		if (buff->content_size == (size_t)fd)
-			// read to current node
-			return(read_line(&buff, line));
+		if (files->content_size == (size_t)fd)
+			return(read_line(&files, line));
 	}
-	// OKAY, I haven't found any node (but ) - so let's add!
-	//
-	ft_lstadd(&buff, ft_lstnew(&buff, fd));
-	return(read_line(&buff, line));
-
-	return (1);
+	ft_lstadd(&files, ft_lstnew(ft_strnew(BUFF_SIZE), (size_t)fd));
+	return(read_line(&files, line));
 }
 
 int			main(void)
 {
-	char	line[1000];
+	char	line[BUFF_SIZE + 1];
 	int		fd;
+	int		ret;
 
+	ret = -100;
 	fd = open("get_next_line.h", O_RDONLY);
-	get_next_line(fd, (char**)&line);
+	ret = get_next_line(fd, (char**)&line);
 	printf("%s\n", line);
 	return (0);
 }
