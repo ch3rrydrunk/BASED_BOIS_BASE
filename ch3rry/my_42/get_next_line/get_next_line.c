0100308c12123543be3fb00858fef26413069a75
Sergey Kharlamov <ch3rryhq@Sergeys-MacBook-Air.local>
2019-05-20 08:07:28 +0300

    get_next_line :  nice reworking -> main body done up to copying, get_thefile in development

--- ch3rry/my_42/get_next_line/get_next_line.c
+++ ch3rry/my_42/get_next_line/get_next_line.c
@@ -6,25 +6,51 @@
 /*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 21:45:25 by caellis           #+#    #+#             */
-/*   Updated: 2019/05/17 02:04:56 by caellis          ###   ########.fr       */
+/*   Updated: 2019/05/20 08:06:27 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>	//DELETE_ME
 
-int			get_next_line(const int fd, char **line)
+static t_list	*get_thefile(t_list **files, int fd)
 {
-	static t_list	*files;
-	
-	return (0);
+
+	return (NULL);
 }
 
-int			main(void)
+int				get_next_line(const int fd, char **line)
 {
-	char	line[BUFF_SIZE + 1];
-	int		fd;
-	int		ret;
+	static t_list	*files;
+	t_list			*cue;
+	char			buff[BUFF_SIZE + 1];
+	char			*tmp;
+	size_t			rb;
+
+	if (!line || fd < 0 || BUFF_SIZE < 1 \
+				||read(fd, buff, 0) < 0 || (cue = get_thefile(&files, fd)))
+		return (-1);
+	while ((rb = read(fd, buff, BUFF_SIZE)))
+	{
+		buff[rb] = '\0';
+		if (!(cue->content = ft_strjoin(cue->content, buff)))
+			return (-1);
+		if ((tmp = ft_strchr(cue->content, (int)'\n')))
+			break ;
+	}
+	if (rb < BUFF_SIZE && !cue->content)
+		return (0);
+	// copy here - if we are here were are copying and returning 1
+	
+	//
+	return (1);
+}
+
+int				main(void)
+{
+	char		line[BUFF_SIZE + 1];
+	int			fd;
+	int			ret;
 
 	ret = -100;
 	fd = open("get_next_line.h", O_RDONLY);
