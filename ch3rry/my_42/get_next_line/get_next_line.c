22facd47284706578132d0c04bb9220ff4e36b10
Sergey Kharlamov <ch3rryhq@Sergeys-MacBook-Air.local>
2019-05-20 09:57:16 +0300

    okay, we onto smthng get_next_line : main body PUMPED, ft_count_char static added. time for get_thefile

--- ch3rry/my_42/get_next_line/get_next_line.c
+++ ch3rry/my_42/get_next_line/get_next_line.c
@@ -6,12 +6,34 @@
 /*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 21:45:25 by caellis           #+#    #+#             */
-/*   Updated: 2019/05/20 08:06:27 by caellis          ###   ########.fr       */
+/*   Updated: 2019/05/20 09:55:47 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>	//DELETE_ME
+
+static size_t	ft_count_char(void **s, int c, size_t *t_rb)
+{
+	char	*buff;
+	char	waldo;
+	size_t	size;
+	size_t	rb;
+
+	buff = (char *)*s;
+	waldo = (char)c;
+	size = ft_strlen(buff) + 1;
+	rb = 0;
+	while (size--)
+	{
+		if (*buff == waldo)
+			return (rb);
+		buff++;
+		rb++;
+		(*t_rb)++;
+	}
+	return (rb);
+}
 
 static t_list	*get_thefile(t_list **files, int fd)
 {
@@ -24,8 +46,8 @@ int				get_next_line(const int fd, char **line)
 	static t_list	*files;
 	t_list			*cue;
 	char			buff[BUFF_SIZE + 1];
-	char			*tmp;
 	size_t			rb;
+	size_t			t_rb;
 
 	if (!line || fd < 0 || BUFF_SIZE < 1 \
 				||read(fd, buff, 0) < 0 || (cue = get_thefile(&files, fd)))
@@ -35,14 +57,17 @@ int				get_next_line(const int fd, char **line)
 		buff[rb] = '\0';
 		if (!(cue->content = ft_strjoin(cue->content, buff)))
 			return (-1);
-		if ((tmp = ft_strchr(cue->content, (int)'\n')))
-			break ;
+		if (ft_count_char(&cue->content, (int)'\n', &t_rb) < rb)
+			break;
+		t_rb += rb;
 	}
-	if (rb < BUFF_SIZE && !cue->content)
+	*line = ft_memccpy((void *)*line, cue->content, (int)'\n', t_rb);
+	cue->content += t_rb;
+	if (!t_rb && !cue->content)
+	{
+		ft_strclr(cue->content);
 		return (0);
-	// copy here - if we are here were are copying and returning 1
-	
-	//
+	}
 	return (1);
 }
 
