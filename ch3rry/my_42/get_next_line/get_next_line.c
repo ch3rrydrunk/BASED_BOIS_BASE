b797735961389dda393374fc142492e5df50faf7
Sergey Kharlamov <ch3rryhq@Sergeys-MacBook-Air.local>
2019-05-21 13:08:13 +0300

    latenite gnl

--- ch3rry/my_42/get_next_line/get_next_line.c
+++ ch3rry/my_42/get_next_line/get_next_line.c
@@ -6,36 +6,41 @@
 /*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 21:45:25 by caellis           #+#    #+#             */
-/*   Updated: 2019/05/20 12:12:46 by caellis          ###   ########.fr       */
+/*   Updated: 2019/05/21 13:07:06 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>	//DELETE_ME
 
-static size_t	ft_count_char(void **s, int c, size_t *t_rb)
+static size_t	readc_to_lst(t_list **files, int fd, int c)
 {
-	char	*buff;
-	char	waldo;
-	size_t	size;
-	size_t	rb;
+	t_list		*cue;
+	char		*buff;
+	size_t		rb;
+	size_t		t_rb;
 
-	buff = (char *)*s;
-	waldo = (char)c;
-	size = ft_strlen(buff) + 1;
-	rb = 0;
-	while (size--)
+	if (!files)
+		
+	cue = *files;
+	t_rb = 0;
+	while ((rb = read(fd, buff, BUFF_SIZE)))
 	{
-		if (*buff == waldo)
-			return (rb);
-		buff++;
-		rb++;
-		(*t_rb)++;
+		buff[rb] = '\0';
+		if (!(cue->content = ft_strjoin(cue->content, buff)))
+			return (-1);
+		if (buff = ft_strchr(cue->content, c))
+		{
+			while (&((cue->content)++) != buff)
+				t_rb++;
+			break ;
+		}
+		t_rb += rb;
 	}
-	return (rb);
+	return (t_rb);
 }
 
-static t_list	*get_thefile(t_list **files, int fd)
+static t_list	*get_file(t_list **files, int fd)
 {
 	t_list	*buff;
 	char	*newstr;
@@ -63,22 +68,15 @@ int				get_next_line(const int fd, char **line)
 {
 	static t_list	*files;
 	t_list			*cue;
-	char			buff[BUFF_SIZE + 1];
-	size_t			rb;
 	size_t			t_rb;
 
-	if (!line || fd < 0 || BUFF_SIZE < 1 ||read(fd, buff, 0) < 0 || (cue = get_thefile(&files, fd)))
+	if (!line || fd < 0 || BUFF_SIZE < 1 \
+							|| !(cue = get_file(&files, fd)))
 		return (-1);
-	while ((rb = read(fd, buff, BUFF_SIZE)))
-	{
-		buff[rb] = '\0';
-		if (!(cue->content = ft_strjoin(cue->content, buff)))
-			return (-1);
-		if (ft_count_char(&cue->content, (int)'\n', &t_rb) < rb)
-			break;
-		t_rb += rb;
-	}
-	*line = ft_memccpy((void *)*line, cue->content, (int)'\n', t_rb);
+	if ((t_rb = readc_to_lst(&cue, fd, (int)'\n')) < 0)
+		return (-1);
+	
+	ft_memccpy(*line, cue->content, (int)'\n', t_rb);
 	cue->content += t_rb;
 	if (!t_rb && !cue->content)
 	{
@@ -90,13 +88,41 @@ int				get_next_line(const int fd, char **line)
 
 int				main(void)
 {
-	char		line[BUFF_SIZE + 1];
+	char		*line;
 	int			fd;
 	int			ret;
 
+	line = ft_strnew(BUFF_SIZE * 4);
 	ret = -100;
-	fd = open("get_next_line.h", O_RDONLY);
-	ret = get_next_line(fd, (char**)&line);
+	fd = open("get_next_line.c", O_RDONLY);
+	while(ret = get_next_line(fd, &line))
+	{
+		printf("%s\n", line);
+	}
+	printf("That's it?\n");
+	get_next_line(fd, &line);
 	printf("%s\n", line);
 	return (0);
+}
+
+static size_t	ft_count_char(void **s, int c, size_t *t_rb)
+{
+	char	*buff;
+	char	waldo;
+	size_t	size;
+	size_t	rb;
+
+	buff = (char *)*s;
+	waldo = (char)c;
+	size = ft_strlen(buff) + 1;
+	rb = 0;
+	while (size--)
+	{
+		if (*buff == waldo)
+			return (rb);
+		buff++;
+		rb++;
+		(*t_rb)++;
+	}
+	return (-1);
 }
