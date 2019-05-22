53e37c23f3a8d092419d470feadbcc8bba9a2336
Sergey Kharlamov <ch3rryhq@Sergeys-MacBook-Air.local>
2019-05-23 02:34:44 +0300

    GNL : IT REALLY DAMN WORKS!

--- ch3rry/my_42/get_next_line/get_next_line.c
+++ ch3rry/my_42/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/05 21:45:25 by caellis           #+#    #+#             */
-/*   Updated: 2019/05/22 19:29:16 by caellis          ###   ########.fr       */
+/*   Updated: 2019/05/23 02:34:24 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,31 +68,58 @@ static t_file	*ft_get_file(t_file **files, int fd)
 	buff = ft_new_file("", fd);
 	ft_add_file(files, buff);
 	return (*files);
+}
 
+static long	ft_file2line_copy(char **line, t_file **cue)
+{
+	char	*file;
+	char	*buff;
+	long	rb;
+
+	file = (*cue)->content;
+	rb = 0;
+	while (*file++)
+	{
+		if (*(file - 1) == (char)'\n')
+			break ;
+		rb++;
+	}
+	if (!(buff = ft_strdup(file)) || !(*line = ft_strnew(rb)))
+		return (-1);
+	*line = (char *)ft_memcpy(*line, (*cue)->content, rb);
+	ft_strdel(&(*cue)->content);
+	(*cue)->content = buff;
+	return (rb);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	static t_file	*files;
 	t_file			*cue;
-	char			*buff;
+	char			buff[BUFF_SIZE + 1];
 	char			*temp;
-	size_t			rb;
+	long			rb;
 
+	ft_bzero((void *)buff, BUFF_SIZE + 1);
 	if (!line || fd < 0 || BUFF_SIZE < 1 || !(cue = ft_get_file(&files, fd)) \
-				|| read(fd, buff, 0) < 0 || !(buff = ft_strnew(BUFF_SIZE + 1)))
+				|| read(fd, buff, 0) < 0)
 		return (-1);
 	while ((rb = read(fd, buff, BUFF_SIZE)))
 	{
 		if (!(temp = ft_strjoin(cue->content, buff)))
-		{
-			ft_strdel(&cue->content);
 			return (-1);
-		}
+		ft_strdel(&cue->content);
+		cue->content = temp;
 		if (ft_strchr(cue->content, (int)'\n'))
 			break;
 	}
-
+	if (!(cue->content) || !*(cue->content))
+	{
+		ft_strdel(&cue->content);
+		return (0);
+	}
+	if ((rb = ft_file2line_copy(line, &cue)) < 0)
+		return (-1);
 	return (1);
 }
 
@@ -103,15 +130,32 @@ int				main(void)
 	int			ret;
 
 	ret = -100;
-	fd = open("line_less_than_buff.test", O_RDONLY);
+	fd = open("get_next_line.c", O_RDONLY);
 	while((ret = get_next_line(fd, &line)))
 	{
+		if (ret == -1)
+		{
+			printf(":(\n");
+			exit (-1);
+		}
 		printf("%s\n", line);
-		free(line);
 	}
-	printf("That's it?\n");
-	get_next_line(fd, &line);
-	printf("%s\n", line);
+	printf("\n\n\n\n\nThat's it?\n\n\n\n\n");
+
+	fd = open("author", O_RDONLY);
+	while((ret = get_next_line(fd, &line)))
+	{
+		if (ret == -1)
+		{
+			printf(":(\n");
+			exit (-1);
+		}
+		printf("%s\n", line);
+	}
+	printf("\n\n\n\n\nThat's it?\n\n\n\n\n");
+
+	ret = get_next_line(fd, &line);
+	printf("This is bytes read from finished file: %i\n", ret);
 	return (0);
 }
 
