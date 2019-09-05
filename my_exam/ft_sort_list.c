31874f6a698e909651921999d79cb9b98a06fd32
ch3rrydrunk <serkhar@gmail.com>
2019-09-05 13:19:42 +0300

    my_exam: sort_list

--- my_exam/ft_sort_list.c
+++ my_exam/ft_sort_list.c
@@ -6,18 +6,21 @@
 /*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 18:50:29 by caellis           #+#    #+#             */
-/*   Updated: 2019/09/03 21:51:42 by caellis          ###   ########.fr       */
+/*   Updated: 2019/09/05 13:19:03 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
 typedef	struct		s_list
 {
 	int		data;
 	struct s_list	*next;
 }			t_list;
+*/
 
 int ascending(int a, int b)
 {
@@ -29,8 +32,35 @@ int descending(int a, int b)
 	return (a >= b);
 }
 
-t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+void	ft_swap(t_list *a, t_list *b)
 {
+	t_list	*c;
+
+	c = a;
+	a = b;
+	b = c;
+}
+
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
+{
+	t_list	*cue = lst;
+	t_list	*buff;
+	
+	if (lst)
+	{
+		while (cue)
+		{
+			buff = lst->next;
+			while (buff)
+			{
+				if (!cmp(cue->data, buff->data))
+					ft_swap(cue, buff);
+				buff = buff->next;
+			}
+			cue = cue->next;
+		}
+		
+	}
 	return (lst);
 }
 
