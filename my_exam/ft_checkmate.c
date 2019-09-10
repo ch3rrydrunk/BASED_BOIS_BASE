a078d5c12a14eec6f18ccc131c2f263042ff9fcc
Caryl Ellis <caellis@mi-t5.21-school.ru>
2019-09-10 12:25:57 +0300

    checkmate wip

--- my_exam/ft_checkmate.c
+++ my_exam/ft_checkmate.c
@@ -6,7 +6,7 @@
 /*   By: caellis <caellis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 18:31:02 by caellis           #+#    #+#             */
-/*   Updated: 2019/09/09 15:52:45 by caellis          ###   ########.fr       */
+/*   Updated: 2019/09/10 12:21:54 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,55 @@
 #include <unistd.h>
 #include "checkmate.h"
 
+void	ft_bzero(void *mem, size_t size)
+{
+	uint8_t		*ptr = (uint8_t *)mem;
+
+	while (size--)
+		*ptr++ = 0U;
+}
+
+char	checkDiags(char **board, t_pos king, int rows)
+{
+	return (0);
+}
+
+char	checkAxes(char **board, t_pos king, int rows)
+{
+	int		i = 0;
+	t_pos	possible[rows << 2];
+	t_pos	*cue = possible;
+
+	ft_bzero(possible, sizeof(t_pos) * (rows << 2));
+	while (i < rows)
+	{
+		if (board[king.i_row][i] == ROOK)
+		{
+			cue->i_row = king.i_row;
+			cue->j_col = i;
+			cue++;
+		}
+		if (board[i][king.j_col] == ROOK)
+		{
+			cue->i_row = i;
+			cue->j_col = king.j_col;
+			cue++;
+		}
+	}
+	if (cue != possible && (i = 0))
+	{
+		
+	}
+	return (0);
+}
+
 char	checkKing(char **board, t_pos king, int rows)
 {
 	int		i = 0;
 
-	while ()
+	if (checkAxes(board, king, rows) || checkDiags(board, king, rows))
+		return (1);
+	return (0);
 }
 
 void	checkmate(int rows, char **board)
