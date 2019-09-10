11365e5e43bd2fc97f1619e4d2d8f81e30046dba
ch3rrydrunk <serkhar@gmail.com>
2019-09-11 00:00:22 +0300

    pwnable.kr : col.c keygen upd

--- my_exam/pwnable.kr/col.c
+++ my_exam/pwnable.kr/col.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-unsigned long hashcode = 0x21DD09EC;
-
-// Okay, so hashcode is array int's of ~value (hashcode / 5)
-// (hashcode / 5) (prettified to include only ASCII chars) is 
-char	*cracker(unsigned long hashcode, char *res)
+unsigned long hashcode = 0x21DD09EC; // == 4 * 142,033,531 == 568134124
+// "0000" == 808464432
+//Key Generator v 0.2
+char	*cracker(char *res)
 {
-	int		*ip = (int *)res;
-	int		i = 0;
+	int	        	*ip = (int *)res;
+    char            *cue = res;
+	int		        i = 0;
+    unsigned int             a, b, c, d, e;
+    unsigned long   checksum = 0;
+    char            gotcha = 0;
 
-	while (i < 4)
-	{
-		*ip++ = (int)(hashcode / 5);
-		i++;
-	}
-	*ip = (int)(hashcode / 5) + (int)(hashcode % 5);
-	return (res);
+	while (!gotcha)
+    {
+        ip = (int *)res;
+        while ((unsigned long)(a + b + c + d + e) != hashcode)
+        {
+            a = 808464432, b = 808464432, c = 808464432, d = 808464432, e = rand();
+            printf("%lu\n", (unsigned long)(a + b + c + d + e));
+        }
+        ip[0] = a, ip[1] = b, ip[2] = c, ip[3] = d, ip[4] = e;
+        while (i < 20)
+        {
+            gotcha = 1;
+            if (cue[i] < 33 || cue[i] > 125)
+                gotcha = 0;
+            i++;
+        }
+    }
+    return (res);
 }
 
 unsigned long check_password(const char* p){
@@ -31,11 +46,11 @@ unsigned long check_password(const char* p){
 }
 
 int main(int argc, char* argv[]){
-	char	pass[21];
+	char	pass[21] = {0};
 
-	printf("%s\n", cracker(hashcode, pass));
 	if(argc<2){
 		printf("usage : %s [passcode]\n", argv[0]);
+        printf("is this it {%s}?\n", cracker(pass));
 		return 0;
 	}
 	if(strlen(argv[1]) != 20){
@@ -43,8 +58,9 @@ int main(int argc, char* argv[]){
 		return 0;
 	}
 
-	if(hashcode == check_password(argv[1])){
+	if(hashcode == check_password(cracker(pass))){
 		system("/bin/cat flag");
+        printf("is this it {%s}?\n", pass);
 		return 0;
 	}
 	else
