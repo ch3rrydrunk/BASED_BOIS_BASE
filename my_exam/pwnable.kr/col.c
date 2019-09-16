c78139ac4fe32451b2a3360748b8f546d3960f50
Caryl Ellis <caellis@mi-t5.21-school.ru>
2019-09-16 15:21:11 +0300

    pwnable.kr added

--- my_exam/pwnable.kr/col.c
+++ my_exam/pwnable.kr/col.c
@@ -3,37 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-unsigned long hashcode = 0x21DD09EC; // == 4 * 142,033,531 == 568134124
-// "0000" == 808464432
-//Key Generator v 0.2
-char	*cracker(char *res)
-{
-	int	        	*ip = (int *)res;
-    char            *cue = res;
-	int		        i = 0;
-    unsigned int    a, b, c, d, e;
-    unsigned long   checksum = 0;
-    char            gotcha = 0;
-
-	while (!gotcha)
-    {
-        ip = (int *)res;
-        while ((unsigned long)(a + b + c + d + e) != hashcode)
-        {
-            a = 808464432, b = 808464432, c = 808464432, d = rand(), e = rand();
-            printf("%lu\n", (unsigned long)(a + b + c + d + e));
-        }
-        ip[0] = a, ip[1] = b, ip[2] = c, ip[3] = d, ip[4] = e;
-        while (i < 20)
-        {
-            gotcha = 1;
-            if (cue[i] < 33 || cue[i] > 125)
-                gotcha = 0;
-            i++;
-        }
-    }
-    return (res);
-}
+unsigned long hashcode = 0x21DD09EC; // == 0x6C5CEC8 * 4 + (0x6C5CEC8 + (0x21DD09EC % 5))
+// The system is most likely Little Endian, therefeore we should reverse our ints
+// (check with python -c "import sys; print(0 if sys.byteorder=='big' else 1)")
+// PWN it with:
+// $ ./col `python -c "print('\xc8\xce\xc5\x06' * 4 + '\xcc\xce\xc5\x06')"`
 
 unsigned long check_password(const char* p){
 	int* ip = (int*)p;
@@ -50,7 +24,6 @@ int main(int argc, char* argv[]){
 
 	if(argc<2){
 		printf("usage : %s [passcode]\n", argv[0]);
-        printf("is this it {%s}?\n", cracker(pass));
 		return 0;
 	}
 	if(strlen(argv[1]) != 20){
@@ -58,9 +31,8 @@ int main(int argc, char* argv[]){
 		return 0;
 	}
 
-	if(hashcode == check_password(cracker(pass))){
+	if(hashcode == check_password(argv[1])){
 		system("/bin/cat flag");
-        printf("is this it {%s}?\n", pass);
 		return 0;
 	}
 	else
