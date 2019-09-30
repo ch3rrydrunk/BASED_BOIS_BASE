f682d3c7e6373726d34f7a3351ebe65f9f1a7d9b
Caryl Ellis <caellis@mi-t3.21-school.ru>
2019-09-30 17:55:35 +0300

    fdf: fdf.c scaling added

--- fdf/src/fdf.c
+++ fdf/src/fdf.c
@@ -6,13 +6,21 @@
 /*   By: caellis <caellis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 16:01:15 by caellis           #+#    #+#             */
-/*   Updated: 2019/09/30 13:38:26 by caellis          ###   ########.fr       */
+/*   Updated: 2019/09/30 17:32:53 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int8_t			*read_map(t_image *map)
+t_vec			*scaled_img(t_image *map)
+{
+	int32_t		s_factor;
+
+	s_factor = (map->height * WIN_W) / (map->width * WIN_H);
+	return (smul_vec(map->img, s_factor));
+}
+
+t_vec			*read_map(t_image *map)
 {
 	char	*line;
 	char	**split;
@@ -37,7 +45,7 @@ int8_t			*read_map(t_image *map)
 	}
 	if (!map->img->next)
 		return (NULL);
-	return(map->img);
+	return(scaled_img(map));
 }
 
 int8_t			output_fdf(t_image *map, void *x11)
@@ -56,10 +64,15 @@ int32_t			main(int32_t ac, char **av)
 	ft_bzero((void *)&map, sizeof(t_image));
 	if (ac == 2)
 	{
-		ZERO_IF_ERROR((mlx_ptr = mlx_init()))
+		ZERO_IF_ERROR(mlx_ptr = mlx_init())
 		ZERO_IF_ERROR((map.fd = open(av[1], O_RDONLY)))
 		ZERO_IF_ERROR(map.img = read_map(&map))
-		ZERO_IF_ERROR(output_fdf(&map, mlx_ptr))
+		//ZERO_IF_ERROR(output_fdf(&map, mlx_ptr))
+		while (map.img->next)
+		{
+			mlx_pixel_put(mlx_ptr, mlx_new_window(mlx_ptr, WIN_W, WIN_H, "derp"), map.img->x, map.img->y, 0x14278d);
+			map.img = map.img->next;
+		}
 		mlx_loop(mlx_ptr);
 		close(map.fd);
 		// free(map and stuff in map)
