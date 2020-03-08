002ab6648ff11583696bfb4cd430f0bb0ddf9ef5
Caryl Ellis <caellis@mi-u3.21-school.ru>
2020-03-08 18:27:41 +0300

    push_swap: header update

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ch3rryhq <ch3rryhq@student.42.fr>          +#+  +:+       +#+        */
+/*   By: caellis <caellis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 18:31:50 by ch3rryhq          #+#    #+#             */
-/*   Updated: 2020/01/27 16:17:11 by ch3rryhq         ###   ########.fr       */
+/*   Updated: 2020/03/08 17:09:05 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,25 @@
 # include "ft_printf.h"
 
 # define STACK_SIZE 1024
+# define MAX_LINE 90
+
+# define STACK_A 0
+# define STACK_B 1
+# define SIZE 0
+# define BOTTOM 1
+# define REVROTATE 0
+# define ROTATE 1
+
+# define ERR_OVERFLOW "Error: Stack overflow\n"
+# define ERR_BADCOMMAND "Error: Incorrect command received\n"
+# define ERR_BADINPUT "Error: Incorrect input\n"
+
+int     st_push(int stack[], int val);
+int     *st_pop(int stack[]);
+
+
+void     com_swap(int stack[]);
+void     com_push(int from[], int to[]);
+void     com_rotate(int stack[], int order);
 
 #endif
