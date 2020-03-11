bc0ce19cbca901085fb8e2365ced3b5f972a0626
ch3rrydrunk <ch3rryhq@ch3rryhq-2.local>
2020-03-11 22:07:09 +0300

    push_swap: stacks moved from arrays to double linked lists

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caellis <caellis@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ch3rryhq <ch3rryhq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 18:31:50 by ch3rryhq          #+#    #+#             */
-/*   Updated: 2020/03/08 18:46:47 by caellis          ###   ########.fr       */
+/*   Updated: 2020/03/11 22:06:58 by ch3rryhq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,22 @@
 # define REVROTATE 0
 # define ROTATE 1
 
+# define PSM_VERBOSE (1u << 0u)
+# define PSM_PRINT (1u << 1u)
+# define PSM_INDEX (1u << 2u)
+
 # define ERR_OVERFLOW "Error: Stack overflow\n"
 # define ERR_BADCOMMAND "Error: Incorrect command received\n"
 # define ERR_BADINPUT "Error: Incorrect input\n"
+
+typedef struct		s_stack
+{
+	int32_t			n;
+	int32_t			index;
+    int8_t          tag;
+    struct s_list	*prev;
+	struct s_list	*next;
+}					t_stack;
 
 int     st_push(int stack[], int val);
 int     *st_pop(int stack[]);
@@ -45,6 +58,8 @@ void    com_swap(int stack[]);
 void    com_push(int from[], int to[]);
 void    com_rotate(int stack[], int order);
 
-void    print_stack(int stack[]);
+t_stack *make_stack(int *tab, int32_t size, int8_t tag);
+void    free_stack(t_stack *s, int32_t size);
+void	stack_util(t_stack *stack, int32_t *tab, int32_t size, int8_t mode);
 
 #endif
--- push_swap/src/utils.c
+++ push_swap/src/utils.c
@@ -3,19 +3,82 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caellis <caellis@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ch3rryhq <ch3rryhq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 18:45:17 by caellis           #+#    #+#             */
-/*   Updated: 2020/03/08 18:48:06 by caellis          ###   ########.fr       */
+/*   Updated: 2020/03/11 22:05:57 by ch3rryhq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    print_stack(int stack[])
+static t_stack	*new_stack(int n)
 {
-    while (stack[SIZE])
+	t_stack *s;
+
+	s = (t_stack *)malloc(sizeof(t_stack));
+	s->n = n;
+	s->index = 0;
+	s->next = NULL;
+	s->prev = NULL;
+	return (s);
+}
+
+void			free_stack(t_stack *s, int32_t size)
+{
+	while (size--)
+	{
+		s = s->next;
+		free(s->prev);
+	}
+	free(s);
+}
+
+t_stack			*make_stack(int *tab, int32_t size, int8_t tag)
+{
+	t_stack *stack;
+	t_stack *top;
+
+	stack = new_stack(*tab++);
+	top = stack;
+	stack->tag = tag;
+	while (size--)
+	{
+		stack->next = new_stack(*tab++);
+		stack->next->prev = stack;
+		stack->next->tag = tag;
+		stack = stack->next;
+	}
+	stack->next = top;
+	top->prev = stack;
+	return (top);
+}
+
+void			stack_util(t_stack *stack, int32_t *tab, int32_t size, int8_t mode)
+{
+	int32_t i;
+
+	i = 1;
+    if (mode & PSM_INDEX)
     {
-        ft_printf("%d\n", *st_pop(stack));
+        if (mode & PSM_VERBOSE)
+            ft_printf("+++INITIATING INDEXATION+++\n");
+	    while (size--)
+	    {
+		    while (stack->n != *tab)
+			    stack = stack->next;
+            if ((mode & PSM_VERBOSE) && !(mode & PSM_PRINT))
+                ft_printf("stack num: {%d}; index: [%d];\n", *tab, i);
+		    stack->index = i++;
+		    tab++;
+	    }
+    }
+    if (mode & PSM_PRINT)
+    {
+        while (size--)
+        {
+            ft_printf("stack num: {%d}; index: [%d];\n", *stack->n, *stack->index);
+            stack = stack->next;
+        }
     }
 }
