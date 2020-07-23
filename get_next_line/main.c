aaa47f80b2527aa61f3e3156d9d225a9e7943e68
underwear <thisfil@ya.ru>
2020-07-23 13:06:01 +0300

    Update main.c

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -17,29 +17,36 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
-int				get_next_line(int fd, char **line);
+int get_next_line(int fd, char **line);
 
-int		main(int ac, char **av)
+int main(int ac, char **av)
 {
-	char 	*line;
-	int		out;
-	int		p[2];
-	char 	*str;
-	int		len = 50;
+	char *line;
+	int out;
+	int p[2];
+	char *str;
+	int len = 50;
 
-	str = (char *)malloc(1000 * 1000);
+	str = (char *) malloc (1000 * 1000);
 	*str = '\0';
-	while (len--)
+	
+	while (len--) {
 		strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+	}
+	
 	out = dup(1);
 	pipe(p);
 	dup2(p[1], 1);
 
-	if (str)
+	if (str) {
 		write(1, str, strlen(str));
+	}
+	
 	close(p[1]);
 	dup2(out, 1);
+	
 	get_next_line(p[0], &line);
+	
 	ft_putstr(line);
 	ft_putstr("\n\n\n");
 	ft_putstr(str);
